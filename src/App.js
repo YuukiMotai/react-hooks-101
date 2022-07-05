c967bedd0f3c7b74fde17bfad9262e17efc40cc2
茂田井雄輝 <motaiyuuki@motaiyuukinoMacBook-Air.local>
2022-07-05 21:26:43 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment2 = () => setCount(previousCount => previousCount + 1);
-  const decrement2 = () => setCount(previousCount => previousCount - 1);
-
-  const reset = () => setCount(0);
-  const double = () => setCount(count * 2);
-  const devide = () => setCount(count / 2);
-
-  const devide2 = () => setCount(previousCount => {
-      if(previousCount % 3 === 0){
-        return previousCount /3
-      }else{
-        return previousCount
-      }
-  })
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
-    <React.Fragment>
-      <div>
-        This is a template for React app.<br />
-        count :{count}<br />
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>reset</button>
-        <button onClick={double}>✖️2</button>
-        <button onClick={devide}>÷2</button>
-        <button onClick={devide2}>３の倍数の時だけ３で割る</button>
-      </div>
-    </React.Fragment>
+    <>
+    <p>商品名：{name} / 値段：{price}</p>
+    <button onClick={() => setPrice(price + 1)}>+1</button>
+    <button onClick={() => setPrice(price - 1)}>-1</button>
+    <button onClick={reset}>reset</button>
+    <input value={name} onChange={e=>setName(e.target.value)}></input>
+    </>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price:  1000,
 }
 
 export default App;

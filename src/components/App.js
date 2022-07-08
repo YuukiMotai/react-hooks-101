f5b73b7d3d84fcec35b4636dc07b7548ea767eba
茂田井雄輝 <motaiyuuki@motaiyuukinoMacBook-Air.local>
2022-07-08 13:38:17 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React,{useReducer, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers'
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+
+  const addEvent = e =>{
+    e.preventDefault()
+    //動画講座１３の１０分くらいから解説
+    console.log({title,content})
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      content
+    })
+    setTitle('')
+    setContent('')
+  }
+
   return (
     <div className="container-fruid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <laberl htmlFor="formEventTitle">Title</laberl>
-          <input className="form-control" id="formEventTitle" />
+          <input className="form-control" id="formEventTitle" value={title} 
+                                          onChange={e =>setTitle(e.target.value)} />
         </div>
         <div className="form-group">
           <laberl htmlFor="formEventContent">Content</laberl>
-          <textarea className="form-control" id="formEventContent" />
+          <textarea className="form-control" id="formEventContent" value={content}
+                                             onChange={e =>setContent(e.target.value)} />
 
-          <button className="btn btn-primary">イベントを作成</button>
+          <button className="btn btn-primary" onClick={addEvent}>イベントを作成</button>
           <button className="btn btn-danger">全てのイベントを削除</button>
         </div>
       </form>

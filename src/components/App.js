f7fc9f616e13b136f6ec7668db329a242ff0f954
茂田井雄輝 <motaiyuuki@motaiyuukinoMacBook-Air.local>
2022-07-08 14:33:08 +0900

    display events, refactor and implement remove event button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React,{useReducer, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers'
+import Event from './Event'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
@@ -49,7 +50,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-            
+          {state.map((event) => (<Event event={event} dispatch={dispatch} />))}
         </tbody>
       </table>
     </div>

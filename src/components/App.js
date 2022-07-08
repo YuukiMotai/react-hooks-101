0a045c26df2188da17307244cd7fcb3b7ed61d61
茂田井雄輝 <motaiyuuki@motaiyuukinoMacBook-Air.local>
2022-07-08 09:52:24 +0900

    move App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-    <div>
-      This is a template for React app.
+    <div className="container-fruid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <laberl htmlFor="formEventTitle">Title</laberl>
+          <input className="form-control" id="formEventTitle" />
+        </div>
+        <div className="form-group">
+          <laberl htmlFor="formEventContent">Content</laberl>
+          <textarea className="form-control" id="formEventContent" />
+
+          <button className="btn btn-primary">イベントを作成</button>
+          <button className="btn btn-danger">全てのイベントを削除</button>
+        </div>
+      </form>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+      </table>
     </div>
   );
 }

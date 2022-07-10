817bac778b05ba1955a538303585508cdaf55dc7
茂田井雄輝 <motaiyuuki@motaiyuukinoMacBook-Air.local>
2022-07-10 17:57:24 +0900

    create operation log in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,26 @@
 import React, { useContext } from 'react'
-import { DELETE_EVENT } from '../actions' 
+import { 
+  ADD_OPERATION_LOG,
+  DELETE_EVENT
+ } from '../actions' 
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const Event=({ event })=>{
     const id = event.id
     const {dispatch} = useContext(AppContext)
     const handleClickDeleteButton =()=>{
     const result = window.confirm(`本当に(id=${id})を削除しても良いですか？`)
-    if(result) dispatch({type: DELETE_EVENT, id: id})
+    if(result) {
+      dispatch({type: DELETE_EVENT, id: id})
+
+      dispatch({
+          type: ADD_OPERATION_LOG,
+          description: `イベント(id=${id})を削除しました`,
+          operatedAt: timeCurrentIso8601()
+      })
     }
+  }
 
     return(
       <tr>
